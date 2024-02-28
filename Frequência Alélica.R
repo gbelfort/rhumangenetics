6072a7aef9f5f0573201c3d2a2bfcd8f9e6ecaec
GuilhermeBelfort <159037008+GuilhermeBelfort@users.noreply.github.com>
2024-02-28 16:37:36 -0300

    Update Frequência Alélica.R

--- Frequência Alélica.R
+++ Frequência Alélica.R
@@ -11,16 +11,18 @@
 #
 # Original Author: Guilherme Belfort Almeida, Caique Manochio
 # Contributor(s):
-# Updated by (and date): Guilherme Belfort Almeida 07/02/2024
+# Updated by (and date): Guilherme Belfort Almeida 28/02/2024
 #
 # Dependencies: R
 #
 # Command line:
 
-# RStudio -> Clicar na lupa acima e substituir GENEINPUT pelo nome do gene a ser usado
 
-library(adegenet)
-library(vcfR)
+
+library('vcfR')
+library('adegenet')
+library('tibble')
+library('dplyr')
 
 #importar vcf pro R
 vcf <- read.vcfR(file.choose())
@@ -36,56 +38,52 @@ gt <- extract.gt(vcf, element = "GT")
 transposto <- as.data.frame(t(gt))
 transposto <- tibble::rownames_to_column(transposto,"ID")
 
-View(GENEINPUT)
+#importar correspondência indivíduo-população
+dados_pop_1kgp <- read.csv(file.choose(), sep = "\t")
+dados_pop_1kgp <- dados_pop_1kgp %>%
+  rename(ID = Sample.name)
+#organizar por ID
+Pops1KG <- dados_pop_1kgp[order(dados_pop_1kgp$ID),]
 
-  # Subset the data for the current popCode value
-GENEINPUT[GENEINPUT == 'IBS,IBS'] <- "IBS"
-GENEINPUT[GENEINPUT == 'IBS,MSL'] <- "IBS"
-GENEINPUT[GENEINPUT == '1|0'] <- "0 1"
-GENEINPUT[GENEINPUT == '1|1'] <- "1 1"
-GENEINPUT[GENEINPUT == '0|1'] <- "0 1"
-GENEINPUT[GENEINPUT == '0|0'] <- "0 0"
 
-  popData <- subset(GENEINPUT, popCode == popCode)
-  View(popData)
-  df <- arrange(popData, AFR, EUR, AMR, EAS, SAS)
+merge <- left_join(transposto, Pops1KG, by= "ID")
+
+GENEDADO <- merge%>%
+  select(ID, Population.code, Population.name, everything()) %>%
+  select(-c("Sex","Biosample.ID","Superpopulation.code","Superpopulation.name",
+            "Population.elastic.ID", "Data.collections"))
+GENEDADO <- select(ID, Population.code, Population.name, everything()) %>%
+  select(-c("Sex","Biosample.ID","Superpopulation.code","Superpopulation.name",
+            "Population.elastic.ID", "Data.collections"))
+
+# renomear Population.code e Population.name para popCode e popName. adegenet não funciona se tiver nome de coluna com '.'
+names(GENEDADO)[names(GENEDADO) == "Population.code"] <- "popCode"
+names(GENEDADO)[names(GENEDADO) == "Population.name"] <- "popName"
+# Um indivíduo com pop.code IBS,IBS. Isso seria interpretado como uma população diferente já que uso popCode como base, mudei para IBS.
+GENEDADO[GENEDADO == 'IBS,IBS'] <- "IBS"
+GENEDADO[GENEDADO == 'IBS,MSL'] <- "IBS"
+# Trocar "|" por " "
+GENEDADO[] <- lapply(GENEDADO, function(x) gsub("\\|", " ", x))
+
+View(GENEDADO)
+
+  popData <- subset(GENEDADO, popCode == popCode)
   
-  # Convert the data to genind format
-  popGenind <- df2genind(X = popData[, -c(1, 2, 3)], pop= GENEINPUT$popCode, sep = " ", ploidy = 2, ncode = 2)
+  # Converter dados para genind
+  popGenind <- df2genind(X = popData[, -c(1, 2, 3)], pop= GENEDADO$popCode, sep = " ", ploidy = 2, ncode = 2)
   
-  # Convert the genind object to a genpop object
+  # Converter genind em genpop
   popGenpop <- genind2genpop(popGenind)
   
-  # Compute the frequency table
+  # Fazer a frequência da tabela
   popFreq <- makefreq(popGenpop, quiet = FALSE, missing = NA, truenames = TRUE)
-  View(popFreq)
-  # Print the frequency table to the console
-  cat("\n\n")
-  print(paste("Frequency table for population", popCode))
-  print(popFreq)
   
+  #transformar em dataframe próprio
+  df <- as.data.frame(popFreq)
   
-  #tentar separar com linha nula as superpopulações
-  popFreq2 <- popFreq
- popFreq2 <- rbind(popFreq2[1:26, ], X, popFreq2[- (1:), ])
- View(popFreq2)
 
- data_new <- rbind(data[1:3, ],            # new_row,
-                   data[- (1:3), ])
- data_new 
- View(popFreq2)
-
-X <- c()
-populations_order <- c("ACB", "ASW", "ESN", "GWD", "LWK", "MSL", "YRI", X, "CLM", "MXL", "PEL",
-                       "PUR", X, "CDX", "CHB", "CHS", "JPT", "KHV", X, "CEU", "FIN", "GBR",
-                       "IBS", "TSI", X, "BEB", "GIH", "ITU", "PJL", "STU")
-  popFreq2 <- popFreq2[populations_order, ]
-View(popFreq2)
-    fileName <- paste0(popCode, "_freq.csv", sep = c())
-  write.table(popFreq, file = fileName, sep = ",", col.names = TRUE, quote = FALSE)
   
-  # Write the frequency table to an Excel file
-  library(openxlsx)
-  excelFileName <- paste0(popCode, "_freq.xlsx", sep = c())
-  write.xlsx(popFreq, file = excelFileName, col.names = TRUE)
-  View(popData)
+  # Colocar a tabela de frequência num arquivo excel
+  library(writexl)
+  write.xlsx(df, "GENE.xlsx", col.names = TRUE, row.names = TRUE)
+  
